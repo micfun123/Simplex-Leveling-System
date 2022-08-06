9e4483c9b8453108632d08337961acd2eab486ea
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-08-06 17:03:20 +0100

    file maker

--- SimplexLevlingSystem/setup.py
+++ SimplexLevlingSystem/setup.py
@@ -1,3 +1,6 @@
+from cgi import test
+from logging import exception
+import sqlite3 as sql
 import aiosqlite
 import asyncio
 import os
@@ -12,5 +15,15 @@ def filesetup(path) -> str:
     try:
         open(path + "/Leverling.db", "x")
         print(f"File has been made successfully at the path: {path} /Leverling.db" )
+        try: 
+            print("Setting up database...")
+            db = sql.connect(path + "/Leverling.db")
+            cursor = db.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS leveling(GuildID INTEGER, UserID INTEGER, UserName TEXT, UserXP INTERGER)")
+            db.commit()
+            db.close()
+            print("Database has been set up successfully.")
+        except:
+            print("Filemade but set up failed")
     except FileExistsError as e:
         return e
