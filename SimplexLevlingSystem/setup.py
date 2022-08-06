2cbf7636f388897038fe191de1cb6533d5e3d08e
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-08-06 16:29:39 +0100

    makefiles

--- SimplexLevlingSystem/setup.py
+++ SimplexLevlingSystem/setup.py
@@ -2,12 +2,15 @@ import aiosqlite
 import asyncio
 import os
 
-def filesetup(path):
+def filesetup(path) -> str:
+    """
+    Makes the database and paths if they don't exist. Then sets up the database.
+    """
     print("Setting up files...")
     if not os.path.exists(path):
         os.makedirs(path)
     try:
-        os.mknod(path/'leveling.db')
+        open(path + "/Leverling.db", "x")
+        print(f"File has been made successfully at the path: {path} /Leverling.db" )
     except FileExistsError as e:
         return e
-
